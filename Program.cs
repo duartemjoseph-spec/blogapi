f2207e5ef40334c8a3110503327fcc3e8f4c92d2
Juicebox_joe <DuartemJoseph@gmail.com>
2026-02-27 10:40:24 -0800

    2nd day push

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
+using blogapi.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<BlogItemService>();
+builder.Services.AddScoped<PasswordService>();
+builder.Services.AddScoped<UserService>();
+
+var connectionString = builder.Configuration.GetConnectionString("myBlogString2");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
--- obj/Debug/net9.0/blogapi.AssemblyInfo.cs
+++ obj/Debug/net9.0/blogapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blogapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+395621642e6e1ea7fb28b01e4bc149447ff2b85a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539a6b85bf417575d41bbff189691bfabe0be8b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("blogapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blogapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
